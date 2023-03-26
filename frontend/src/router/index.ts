68b227f0062e18dae2b1e839986bd3edcbbf5860
Kemmie001 <oluwakemisola757@gmail.com>
2023-03-26 11:24:37 +0100

    feat: implemented addproducts page design

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -14,7 +14,20 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/ProductsView.vue"),
+      import(
+        /* webpackChunkName: "about" */ "../views/Products/ProductsView.vue"
+      ),
+  },
+  {
+    path: "/add-product",
+    name: "add-products",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "about" */ "../views/Products/AddProduct.vue"
+      ),
   },
 ];
 
---
+++ frontend/src/views/Products/AddProduct.vue
@@ -0,0 +1,105 @@
+<!-- eslint-disable no-undef -->
+<!-- eslint-disable no-undef -->
+<template>
+  <div class="px-8 py-10">
+    <div class="flex gap-3 items-center">
+      <button @click.prevent="router.go(-1)" class="btn-secondary p-0.5">
+        <img src="../../assets/icons/arrow-left.svg" alt="" />
+      </button>
+      <h5 class="header5">Add product</h5>
+    </div>
+    <ProductUploadNav />
+    <ProductDetailsForm
+      v-model:product-name="productInfo.name"
+      v-model:product-description="productInfo.description"
+      v-model:product-category="productInfo.category"
+      v-model:product-quantity="productInfo.quantity"
+      v-model:product-tag="tag"
+      v-model:product-reviewStatus="productInfo.reviewStatus"
+      v-model:product-bust="productInfo.measurement.bust"
+      v-model:product-hip="productInfo.measurement.hip"
+      v-model:product-waist="productInfo.measurement.waist"
+      v-model:product-height="productInfo.measurement.height"
+      :add-tag="addTag"
+      :remove-tag="removeTag"
+      :add-highlight="addHighlight"
+      :remove-highlight="removeHighlight"
+      :productTags="productInfo.tags"
+      :productHighlights="productInfo.highlights"
+      :add-highlight-title="addHighlightTitle"
+      :add-highlight-details="addHighlightDetails"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ProductInfo } from "../../composables/products/productDetails";
+import ProductUploadNav from "@/components/products/AddProducts/ProductUploadNav.vue";
+import { ref } from "vue";
+import type { Ref } from "vue";
+import { useRouter } from "vue-router";
+import ProductDetailsForm from "@/components/products/AddProducts/ProductDetailsForm.vue";
+
+const router = useRouter();
+
+const productInfo: Ref<ProductInfo> = ref({
+  name: "",
+  description: "",
+  category: "",
+  quantity: 0,
+  tags: [],
+  reviewStatus: false,
+  measurement: {
+    bust: 0,
+    height: 0,
+    hip: 0,
+    waist: 0,
+  },
+  highlights: [],
+  price: 0,
+  comparePrice: 0,
+});
+const tag = ref("");
+const addTag = () => {
+  productInfo.value.tags.push(tag.value);
+  tag.value = "";
+};
+const removeTag = (index: number) => {
+  productInfo.value.tags.splice(index, 1);
+};
+let nextId = 0;
+const addHighlight = () => {
+  const highlight = ref({
+    id: nextId++,
+    title: "",
+    details: "",
+  });
+  const newp = productInfo.value.highlights;
+  newp.push(highlight);
+  productInfo.value.highlights = newp;
+};
+const removeHighlight = (index: number) => {
+  productInfo.value.highlights.splice(index, 1);
+};
+const addHighlightTitle = (highlight: any) => {
+  productInfo.value.highlights = productInfo.value.highlights.map(
+    (h, index) => {
+      if (index === highlight.id) {
+        h.value.title = highlight.title;
+      }
+      return h;
+    }
+  );
+};
+const addHighlightDetails = (highlight: any) => {
+  productInfo.value.highlights = productInfo.value.highlights.map(
+    (h, index) => {
+      if (index === highlight.id) {
+        h.value.details = highlight.details;
+      }
+      return h;
+    }
+  );
+};
+</script>
+<style lang="scss"></style>
