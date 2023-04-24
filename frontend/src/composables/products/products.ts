e45c21a1f298be691caef8c88652294a7d8c5967
Kemmie001 <oluwakemisola757@gmail.com>
2023-04-24 09:52:56 +0100

    feat: implemented hide product modal

--- frontend/src/composables/products/products.ts
+++ frontend/src/composables/products/products.ts
@@ -1,17 +1,20 @@
 export const productList = [
   {
+    id: "1",
     name: "Salsa African Attire",
     price: 129,
     uploadDate: "Uploaded Aug 3, 2023",
     status: "Processing",
   },
   {
+    id: "2",
     name: "Salsa African Attire",
     price: 129,
     uploadDate: "Uploaded Aug 3, 2023",
     status: "Draft",
   },
   {
+    id: "3",
     name: "Salsa African Attire",
     price: 129,
     uploadDate: "Uploaded Aug 3, 2023",
@@ -19,12 +22,14 @@ export const productList = [
   },
 
   {
+    id: "4",
     name: "Salsa African Attire",
     price: 129,
     uploadDate: "Uploaded Aug 3, 2023",
     status: "Processing",
   },
   {
+    id: "5",
     name: "Salsa African Attire",
     price: 129,
     uploadDate: "Uploaded Aug 3, 2023",
