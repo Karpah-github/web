aa459273179fa4f55a88630c9a2e40251a09bd42
Kemmie001 <oluwakemisola757@gmail.com>
2023-05-03 15:54:23 +0100

    fix: added notifications

--- frontend/src/components/modals/NotificationCard.vue
+++ frontend/src/components/modals/NotificationCard.vue
@@ -7,11 +7,11 @@ const props = defineProps(["open", "close", "showNotificationModal"]);
 <template>
   <modal :open="props.open" :close="props.close" class="overflow-y-hidden">
     <div class="notification-modal py-2" :class="props.open ? 'show' : ''">
-      <div class="px-2 flex justify-between py-2 border-b border-netrual">
+      <div class="px-2 flex justify-between py-2 border-b border-[#DFDFDF]">
         <h3 class="font-medium">Notifications</h3>
         <p class="text-neutral text-sm font-light">Mark all as read</p>
       </div>
-      <div class="flex items-center justify-center mt-10">
+      <!-- <div class="flex items-center justify-center mt-20">
         <div class="text-center">
           <div class="w-32 mx-auto mb-1">
             <img
@@ -24,6 +24,88 @@ const props = defineProps(["open", "close", "showNotificationModal"]);
             No notification yet
           </p>
         </div>
+      </div> -->
+      <div>
+        <div class="px-4 py-4 flex flex-col gap-5 mb-4">
+          <div class="flex gap-1 items-center">
+            <div class="img mr-2">
+              <img src="../../assets/avatar.svg" alt="avatar" />
+            </div>
+            <div class="">
+              <p class="text-dark">New sale !</p>
+              <p class="font-light text-neutral text-xs">
+                You sold 3 units of XYZ product for $150
+              </p>
+            </div>
+          </div>
+          <div class="flex gap-2 items-center">
+            <div
+              class="w-10 h-10 flex justify-center items-center rounded-full bg-[#FAF4EE]"
+            >
+              <img
+                class="svg-primary"
+                src="../../assets/icons/messages-2.svg"
+                alt="avatar"
+              />
+            </div>
+            <div class="">
+              <p class="text-dark">You have a new message</p>
+              <p class="font-light text-neutral text-xs">
+                A payment of $7.20 has been applied to your financial account.
+              </p>
+            </div>
+          </div>
+          <div class="flex gap-1 items-center">
+            <div class="img mr-2">
+              <img src="../../assets/avatar.svg" alt="avatar" />
+            </div>
+            <div class="">
+              <p class="text-dark">New sale!</p>
+              <p class="font-light text-neutral text-xs">
+                You sold 3 units of XYZ product for $150
+              </p>
+            </div>
+          </div>
+          <div class="flex gap-2 items-center">
+            <div
+              class="w-10 h-10 flex justify-center items-center rounded-full bg-[#FAF4EE]"
+            >
+              <img
+                class="svg-primary"
+                src="../../assets/icons/messages-2.svg"
+                alt="avatar"
+              />
+            </div>
+            <div class="">
+              <p class="text-dark">You have a new message</p>
+              <p class="font-light text-neutral text-xs">
+                A payment of $7.20 has been applied to your financial account.
+              </p>
+            </div>
+          </div>
+          <div class="flex gap-2 items-center">
+            <div
+              class="w-10 h-10 flex justify-center items-center rounded-full bg-[#FAF4EE]"
+            >
+              <img
+                class="svg-primary w-6"
+                src="../../assets/icons/shopping-cart.svg"
+                alt="avatar"
+              />
+            </div>
+            <div class="">
+              <p class="text-dark">
+                New Order received for Salsa African Dress
+              </p>
+              <p class="font-light text-neutral text-xs">
+                You've received a new order for Salsa African dress
+              </p>
+            </div>
+          </div>
+        </div>
+        <router-link to="/" class="text-neutral text-sm pl-2"
+          >See all activities</router-link
+        >
       </div>
     </div>
   </modal>
@@ -39,7 +121,7 @@ const props = defineProps(["open", "close", "showNotificationModal"]);
   z-index: 3;
   position: fixed;
   width: 80%;
-  height: 355px;
+  height: 450px;
   cursor: pointer;
   border-radius: 8px;
   &.show {
