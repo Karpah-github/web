a09bef005db4fd7386c13e8fe146c62437ea1690
juliet-karpah <122952544+juliet-karpah@users.noreply.github.com>
2023-05-12 21:46:18 -0700

    remove

--- backend/auth/src/routes/signin.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const router = express.Router()
-
-router.post('/api/users/signin', (req:any, res:any) => {
-    res.send('hi there!');
-});
-
-export {router as signinRouter}
--- backend/auth/src/routes/signout.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const router = express.Router()
-
-router.post('/api/users/signout', (req:any, res:any) => {
-    res.send('hi there!');
-});
-
-export {router as signoutRouter}
