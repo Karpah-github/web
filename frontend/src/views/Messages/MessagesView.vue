fc0c901dc0f0a7f0582f039fcbc1ef178d7fbd8f
Kemmie001 <oluwakemisola757@gmail.com>
2023-05-15 10:05:33 +0100

    feat: added accept or decline prompt

--- frontend/src/views/Messages/MessagesView.vue
+++ frontend/src/views/Messages/MessagesView.vue
@@ -301,7 +301,7 @@ const openMessageRequest = ref(false);
             </div>
           </div>
         </div>
-        <div class="px-4 mb-4">
+        <div v-if="selectedChat.status === 'accepted'" class="px-4 mb-4">
           <div class="message-input w-full">
             <label htmlFor="image" className="w-10 cursor-pointer">
               <input
@@ -346,6 +346,24 @@ const openMessageRequest = ref(false);
             </span>
           </div>
         </div>
+        <div class="accept-request-box mx-4 py-10 mb-4">
+          <h6 class="text-[#4D4D4D] text-md">
+            {{ selectedChat.username }} wants to send you a message
+          </h6>
+          <p class="text-sm text-neutral py-2 font-light md:w-3/4 mx-auto">
+            Do you want to receive messages from John Doe? Accept this message
+            request to start conversations.
+          </p>
+          <div class="flex justify-center gap-2 mt-3">
+            <button class="btn-secondary px-5 py-2">Cancel</button>
+            <button
+              @click="selectedChat.staus = 'accepted'"
+              class="btn-primary px-5 py-2 rounded-sm text-sm"
+            >
+              Accept Request
+            </button>
+          </div>
+        </div>
       </div>
     </section>
   </div>
@@ -371,5 +389,15 @@ const openMessageRequest = ref(false);
 .svg-neutral-1 {
   filter: invert(33%) sepia(0%) saturate(0%) hue-rotate(252deg) brightness(88%)
     contrast(93%);
+}
+.accept-request-box {
+  background: #ffffff;
+  box-shadow: -20px -20px 25px rgba(16, 24, 40, 0.02),
+    -8px -8px 10px rgba(16, 24, 40, 0.01),
+    20px 20px 25px -5px rgba(16, 24, 40, 0.02),
+    8px 8px 10px -6px rgba(16, 24, 40, 0.01);
+  border-radius: 8px;
+  text-align: center;
+  widows: 100%;
 }
 </style>
