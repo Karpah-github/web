cea6d85834a37bc589f72181a0b8549139b3595b
Kemmie001 <oluwakemisola757@gmail.com>
2023-11-15 21:36:52 +0100

    feat: removed used packages

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+import { createPinia } from "pinia";
 import "./assets/tailwind.css";
 import "./assets/main.scss";
 import CKEditor from "@ckeditor/ckeditor5-vue";
 
-createApp(App).use(store).use(router).use(CKEditor).mount("#app");
+const pinia = createPinia();
+
+createApp(App).use(pinia).use(router).use(CKEditor).mount("#app");
